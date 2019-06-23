3c8fb6c4e1d7eabb1b9f8252a19f36dfc8b46c54
Yury Savin <savinyurii@gmail.com>
2019-06-23 14:29:59 +0300

    Inject on google search page

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -14,20 +14,26 @@ function promisifier(method) {
 }
 
 function promisifyAll(obj, list) {
-  list.forEach(api => bluebird.promisifyAll(obj[api], { promisifier }));
+  list.forEach((api) => bluebird.promisifyAll(obj[api], { promisifier }));
 }
 
 // let chrome extension api support Promise
-promisifyAll(chrome, [
-  'tabs',
-  'windows',
-  'browserAction',
-  'contextMenus'
-]);
-promisifyAll(chrome.storage, [
-  'local',
-]);
+promisifyAll(chrome, ['tabs', 'windows', 'browserAction', 'contextMenus']);
+promisifyAll(chrome.storage, ['local']);
 
 require('./background/contextMenus');
 require('./background/inject');
 require('./background/badge');
+
+const aggregate = require('../../app/utils/aggregate');
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method == 'test') {
+    console.log(request);
+
+    aggregate.queryAggregate(request.host).then((result) => {
+      sendResponse(result);
+    });
+
+    return true;
+  }
+});
