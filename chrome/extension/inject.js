6e5b48d2b62ad3b3342c4b7f6a7c9c5ad09c39bd
Yury Savin <savinyurii@gmail.com>
2019-06-23 14:55:59 +0300

    Show icons on google search page

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -1,21 +1,23 @@
-const aggregate = require('../../app/utils/aggregate');
-
 window.addEventListener('load', () => {
-  let hosts = [];
-  document.querySelectorAll('.srg .r>a').forEach((googleItem) => {
-    const host = new URL(googleItem.href).host;
-
-    chrome.runtime.sendMessage({ method: 'test', host }, function(response) {
-      console.log(response);
-      const posts = response.posts;
-      const text = `[${posts.length} votes]`;
-      const span = document.createElement('span');
-      span.innerText = text;
-      googleItem.appendChild(span);
-    });
+  document.querySelectorAll('#rso .r').forEach((googleRow) => {
+    const h3 = googleRow.querySelector('h3');
+    const a = googleRow.querySelector('a');
+    const host = new URL(a.href).host;
+    if (h3) {
+      chrome.runtime.sendMessage({ method: 'test', host }, function(response) {
+        console.log(response);
+        const posts = response.posts;
+        const average =
+          posts.reduce((acc, post) => acc + parseFloat(post.rating), 0) /
+          posts.length;
+        const text = `[${posts.length} votes, avg. rate ${
+          posts.length > 0 ? average.toFixed(1) : 'NO'
+        }]`;
+        const span = document.createElement('span');
+        span.title = text;
+        span.innerHTML = posts.length === 0 ? '❓' : average > 3 ? '👍' : '👎';
+        h3.appendChild(span);
+      });
+    }
   });
-
-  // (async function() {
-  //   await aggregate.queryAggregate(host);
-  // })()
 });
