c7304a5db26ec69cf4514bf02ba0e2037f1f9968
Yury Savin <savinyurii@gmail.com>
2019-06-28 22:12:45 +0300

    🆙 Upgrade to react@16.8 (closes #5)

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,12 +1,8 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 
 export default class Root extends Component {
-  static propTypes = {
-    store: PropTypes.object.isRequired,
-  };
-
   render() {
     const { store } = this.props;
     return (
