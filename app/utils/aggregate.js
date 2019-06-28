a3db9261deebb4ceff2c34326e360fee724659eb
Yury Savin <savinyurii@gmail.com>
2019-06-28 22:14:40 +0300

    😎 Improve concatination

--- app/utils/aggregate.js
+++ app/utils/aggregate.js
@@ -3,8 +3,8 @@ import * as fls from './fluence';
 
 async function queryAggregate(url) {
   return Promise.all([arw.queryReviews(url), fls.queryReviews(url)]).then(
-    (result) => {
-      const posts = [].concat(result[0]).concat(result[1]);
+    (values) => {
+      const posts = [].concat(...values);
 
       // Sort by date desc
       posts.sort((p1, p2) => p2.timestamp - p1.timestamp);
