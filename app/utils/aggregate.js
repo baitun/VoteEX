b1eb5f737aecdfe8810f70f84e15a27a7d7230d4
Yury Savin <yury@savin.dev>
2019-06-29 20:02:22 +0300

    🧮 Calculate average in aggregate

--- app/utils/aggregate.js
+++ app/utils/aggregate.js
@@ -1,26 +1,36 @@
 import * as arw from './arweave';
 import * as fls from './fluence';
 
-async function queryAggregate(url) {
-  return Promise.all([arw.queryReviews(url), fls.queryReviews(url)]).then(
-    (values) => {
-      const posts = [].concat(...values);
+/**
+ * Get merget reviews from both fluence and arvweave
+ * @param {string} host
+ * @returns {Promise<{count:number, average:number, posts: {}[]>}
+ */
+async function queryAggregate(host) {
+  const promises = [
+    //
+    arw.queryReviews(host),
+    fls.queryReviews(host),
+  ];
+  return Promise.all(promises).then((values) => {
+    const posts = [].concat(...values);
 
-      // Sort by date desc
-      posts.sort((p1, p2) => p2.timestamp - p1.timestamp);
+    // Sort by date desc
+    posts.sort((p1, p2) => p2.timestamp - p1.timestamp);
 
-      const count = posts.length;
+    const count = posts.length;
+    const average =
+      posts.length === 0
+        ? 0
+        : posts.reduce((acc, post) => acc + parseFloat(post.rating), 0) /
+          posts.length;
 
-      return {
-        count,
-        posts,
-      };
-    }
-  );
+    return {
+      count,
+      average,
+      posts,
+    };
+  });
 }
 
-async function x() {
-  return null;
-}
-
-export { queryAggregate, x };
+export { queryAggregate };
