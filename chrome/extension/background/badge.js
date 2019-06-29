d77fa76745f19d07969fbe06b8b21fcfb49f6f57
Yury Savin <yury@savin.dev>
2019-06-29 20:23:19 +0300

    Change badge color and show title on hover

--- chrome/extension/background/badge.js
+++ chrome/extension/background/badge.js
@@ -1,14 +1,23 @@
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  const host = new URL(tab.url).host;
+  if (changeInfo.status !== 'complete') return;
   const aggregate = require('../../../app/utils/aggregate');
 
-  chrome.browserAction.setBadgeText({ text: '?' });
-  aggregate.queryAggregate(host).then(result => {
-    chrome.browserAction.setBadgeText({ text: `${result.count}` });
+  const host = new URL(tab.url).host;
+  console.log('[onTabUpdated]', host, tab);
 
-    const val = {};
-    val.key = `${host}-reviews`;
-    val.value = result;
-    chrome.storage.local.set(val);
+  chrome.browserAction.setBadgeText({ text: '?' });
+  aggregate.queryAggregate(host).then((result) => {
+    const { count, average } = result;
+    const title = `${host}: ${count} votes, avg. rate ${average.toFixed(2)}`;
+    const color = average > 3 ? '#00FF00' : '#FF0000';
+
+    chrome.browserAction.setTitle({ title, tabId });
+    chrome.browserAction.setBadgeText({ text: String(count), tabId });
+    chrome.browserAction.setBadgeBackgroundColor({ color, tabId });
+
+    //   const val = {};
+    //   val.key = `${host}-reviews`;
+    //   val.value = result;
+    //   chrome.storage.local.set(val);
   });
 });
