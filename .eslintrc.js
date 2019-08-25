bff31789495ce75b78146d899842b33a3bde30e8
Yury Savin <yury@savin.dev>
2019-08-25 09:23:36 +0300

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: 'babel-eslint',
+  // parser: 'babel-eslint',
   // extends: 'airbnb',
   globals: {
     chrome: true,
@@ -9,14 +9,14 @@ module.exports = {
     node: true,
   },
   rules: {
-    'react/prefer-stateless-function': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/label-has-for': 0,
-    'consistent-return': 0,
-    'comma-dangle': 0,
-    'spaced-comment': 0,
-    'global-require': 0,
+    // 'react/prefer-stateless-function': 0,
+    // 'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    // 'jsx-a11y/no-static-element-interactions': 0,
+    // 'jsx-a11y/label-has-for': 0,
+    // 'consistent-return': 0,
+    // 'comma-dangle': 0,
+    // 'spaced-comment': 0,
+    // 'global-require': 0,
   },
   plugins: ['react'],
 };
--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
+import { KeyUploader } from '../components/KeyUploader/KeyUploader';
 
 export default class Root extends Component {
   render() {
     const { store } = this.props;
+    console.log('[Root render]', { store });
     return (
       <Provider store={store}>
-        <App />
+        {/* <App /> */}
+        <KeyUploader />
       </Provider>
     );
   }
